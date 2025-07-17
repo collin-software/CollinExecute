99e985df39fce4458807fff546b70f558014514d
Zachary Jones <jones_zachary@outlook.com>
2025-07-17 18:58:47 -0500

    Add project files.

---
+++ CollinExecute/CollinExecute.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CollinExecute
+{
+    static class Utils
+    {
+        public static bool IsWindows() => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        public static bool IsMacOS() => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+        public static bool IsLinux() => RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+    }
+    public class CollinExecute
+    {
+        static bool Shell(string command)
+        {
+
+        }
+    }
+}
